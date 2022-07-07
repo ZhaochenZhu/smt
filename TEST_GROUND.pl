f6aa58bcc7aa930ab6d9828a31f28c9ab33a7ba3
Yucheng Wang <kris_7z@hotmail.com>
2022-07-06 23:44:08 -0700

    Add files via upload

--- TEST_GROUND.pl
+++ TEST_GROUND.pl
@@ -252,6 +252,28 @@ my %h_track_net = ();
 
 my $infileStatus = "init";
 
+sub combine;
+sub combine_sub;
+sub getAvailableNumFinger;
+
+sub getAvailableNumFinger{
+	$width = @_[0];
+	$trackEachPRow = @_[1];
+	# print ("[getAvailableNumFinger]: ");
+	@numFinger = ();
+	for my $i(0 .. $trackEachPRow-1){
+
+		if($width % ($trackEachPRow-$i) == 0){
+			push(@numFinger, $width/($trackEachPRow-$i));
+			# print "$width/($trackEachPRow-$i)";
+			last; #immediately causes the current iteration of a loop to become the last. No further statements are executed, and the loop ends.
+		}
+	}
+	# print "\n";
+    # numFinger only has one elements
+	return @numFinger;
+}
+
 ### Read Inputfile and Build Data Structure
 open (my $in, "./$infile");
 while (<$in>) {
@@ -314,18 +336,23 @@ while (<$in>) {
         }
         elsif ($line =~ /Tracks per Placement Clip\s*= (\d+)/) {
             #$numPTrackH = $1*2;
-	    $numPTrackH = $1; # CFET
+	        $numPTrackH = $1; # CFET
 			$trackEachPRow = $1;
             print "a     # Horizontal Placement Tracks = $numPTrackH\n";
         }
     }
+    my @tmp = getAvailableNumFinger(13, 7);
 
-    ### Infile Status: Instance Info
+    # print Dumper(\@tmp);
+
+    ## Infile Status: Instance Info
     if ($infileStatus eq "inst") {
         if ($line =~ /^i   ins(\S+)\s*(\S+)\s*(\d+)/) {	
 			$instName = "ins".$1;
 			$instType = $2;
 			$instWidth = $3;
+
+            print "Reading Instance Info: $instName, $instType, $instWidth \n";
 
 			my @tmp_finger = ();
 			@tmp_finger = getAvailableNumFinger($instWidth, $trackEachPRow);
@@ -338,12 +365,14 @@ while (<$in>) {
 			}
 			else{
 				# Mark: PMOS Y is set to 0
-				$instY = 0;
+				$instY = 0; # output transistor?
 				#$instY = $numPTrackH - $instWidth/$tmp_finger[0];
 			}
 			push(@inst, [($instName, $instType, $instWidth, $instY)]);
 			### Generate Maximum possible pin arrays for each instances
 			### # of Maximum Possible pin = instWidth * 2 + 1
+            # print('tmp_finger: ', $#tmp_finger, "\n");
+            print('tmp_finger: ', $tmp_finger[$#tmp_finger]*2+1, "\n");
 			for my $i(0 .. ($tmp_finger[$#tmp_finger]*2+1)-1){
 				if($i==0){
 					$h_pin_idx{$instName} = $pinIDX;
@@ -354,6 +383,11 @@ while (<$in>) {
 				}
 				@pin = ("pin$1_$i", "", "t", $trackEachPRow, $i, [@pinYpos], $instName, "");
 				push (@pins, [@pin]);
+                
+                # YW Debug
+                print "Adding current pin to pins: \n";
+                print Dumper(\@pin), "\n";
+
 				$h_pinId_idx{"pin$1_$i"} = $pinIDX;
 				$pinIDX++;
 				$pinTotalIDX++;
@@ -363,4 +397,219 @@ while (<$in>) {
 			$numInstance++;
 		}
     }
+
+    ### Infile Status: pin
+    if ($infileStatus eq "pin") {
+		if ($line =~ /^i   pin(\d+)\s*net(\d+)\s*(\S+)\s*(\S+)\s*(\S+)\s*(\S+)\s*(\S+)/) {
+			$pin_type_IO = $7;
+		}
+		if ($line =~ /^i   pin(\d+)\s*net(\d+)\s*(\S+)\s*(\S+)\s*(\S+)\s*(\S+)/) {
+            $pinName = "pin".$1;
+            $pin_netID = "net".$2; 
+			$pin_instID = $3;
+			$pin_type = $4;
+            $pinIO = $5;
+            $pinLength = $6;
+
+            print "Reading Pin Info: $pinName, $pin_netID, $pin_instID, $pin_type, $pinIO, $pinLength \n";
+
+			my @tmp_finger = ();
+			@tmp_finger = getAvailableNumFinger($inst[$h_inst_idx{$pin_instID}][2], $trackEachPRow);
+			if($pin_instID ne "ext" && $pin_type eq "S"){
+				for my $i(0 .. ($tmp_finger[$#tmp_finger]*2+1)-1){
+					if($i%4==0){
+						$pins[$h_pin_idx{$pin_instID}+$i][1] = $pin_netID;
+						$pins[$h_pin_idx{$pin_instID}+$i][7] = $pin_type;
+						if($i==0){
+							$pins[$h_pin_idx{$pin_instID}+$i][2] = $pinIO;
+						}
+					}
+				}
+			}
+			elsif($pin_instID ne "ext" && $pin_type eq "D"){
+				for my $i(0 .. ($tmp_finger[$#tmp_finger]*2+1)-1){
+					if($i>=2 && ($i-2)%4==0){
+						$pins[$h_pin_idx{$pin_instID}+$i][1] = $pin_netID;
+						$pins[$h_pin_idx{$pin_instID}+$i][7] = $pin_type;
+						if($i==2){
+							$pins[$h_pin_idx{$pin_instID}+$i][2] = $pinIO;
+						}
+					}
+				}
+			}
+			elsif($pin_instID ne "ext" && $pin_type eq "G"){
+				for my $i(0 .. ($tmp_finger[$#tmp_finger]*2+1)-1){
+					if($i>=1 && ($i)%2==1){
+						$pins[$h_pin_idx{$pin_instID}+$i][1] = $pin_netID;
+						$pins[$h_pin_idx{$pin_instID}+$i][7] = $pin_type;
+						if($i==1){
+							$pins[$h_pin_idx{$pin_instID}+$i][2] = $pinIO;
+						}
+					}
+				}
+			}
+			elsif($pin_instID eq "ext"){
+				@pin = ($pinName, $pin_netID, $pinIO, $pinLength, $pinXpos, [@pinYpos], $pin_instID, $pin_type);
+				push (@pins, [@pin]);
+				$h_outpinId_idx{$pinName} = $pinTotalIDX;
+				$pinTotalIDX++;
+				if($pin_type ne "VDD" && $pin_type ne "VSS"){
+					$h_extnets{$2} = 1;
+				}
+				if($pin_type_IO eq "O"){
+#if($pin_type eq "QN" || $pin_type eq "ZN" || $pin_type eq "Q" || $pin_type eq "S" || $pin_type eq "CO" || $pin_type eq "CON"){
+					$h_outnets{$pin_netID} = 1;
+					print "OUTPUT NET : $pin_netID\n";
+				}	
+			} 
+			$h_pin_id{$pin_instID."_".$pin_type} = $2;
+        }
+    }
+
+    ### Infile Status: net
+    if ($infileStatus eq "net") {
+        if ($line =~ /^i   net(\S+)\s*(\d+)PinNet/) {
+			$numNets_org++;
+            $netID = $1;
+            $netName = "net".$netID;
+
+            print("Reading: ", $netID, " ", $netName, "\n");
+
+			my $powerinNet = 0;
+			my $powerNet = "";
+			if(exists($h_pin_net{$netName})){
+				print "$netName => $h_pin_net{$netName}\n";
+				@net = split /\s+/, $h_pin_net{$netName};
+			}
+			else{
+				print "[ERROR] Parsing Net Info : Net Information is not correct!! [$netName]\n";
+				exit(-1);
+			}
+            for my $pinIndex_inNet (0 .. $#net) {
+                # if is an VDD or VSS pins
+				if(exists($h_outpinId_idx{$net[$pinIndex_inNet]}) && ($pins[$h_outpinId_idx{$net[$pinIndex_inNet]}][7] eq "VDD" || $pins[$h_outpinId_idx{$net[$pinIndex_inNet]}][7] eq "VSS")){
+					$powerinNet = 1;
+                    # 
+					$powerNet = $net[$pinIndex_inNet];
+				}
+			}
+			if($powerinNet == 0){
+				$N_pinNets = $#net+1;
+				@pins_inNet = ();
+				my $num_outpin = 0;
+				for my $pinIndex_inNet (0 .. $N_pinNets-1) {
+					push (@pins_inNet, $net[$pinIndex_inNet]);
+				}
+				$source_ofNet = $pins_inNet[$N_pinNets-1];
+				$numSinks = $N_pinNets - 1;
+				@sinks_inNet = ();
+				for my $sinkIndex_inNet (0 .. $numSinks-1) {
+					push (@sinks_inNet, $net[$sinkIndex_inNet]);
+				}
+				$numSinks = $numSinks - $num_outpin;
+				@net = ($netName, $netID, $N_pinNets, $source_ofNet, $numSinks, [@sinks_inNet], [@pins_inNet]);
+				push (@nets, [@net]);
+				#$h_net_idx{$netName} = $idx_nets;
+				$idx_nets++;
+			}
+			else{
+				my $subidx_net = 0;
+				
+				for my $pinIndex_inNet (0 .. $#net) {
+					$h_pin_power{$net[$pinIndex_inNet]} = 1;
+					$N_pinNets = 2;
+					@pins_inNet = ();
+					@sinks_inNet = ();
+					if($net[$pinIndex_inNet] ne $powerNet){
+						push (@pins_inNet, $powerNet);
+						push (@pins_inNet, $net[$pinIndex_inNet]);
+						$source_ofNet = $net[$pinIndex_inNet];
+						$numSinks = 1;
+						push (@sinks_inNet, $powerNet);
+						my @tmpnet = ($netName."_".$subidx_net, $netID."_".$subidx_net, $N_pinNets, $source_ofNet, $numSinks, [@sinks_inNet], [@pins_inNet]);
+						push (@nets, [@tmpnet]);
+						my $tmp_net_name = $netName."_".$subidx_net;
+						#$h_net_idx{$tmp_net_name} = $idx_nets;
+						$h_net_power{$netName."_".$subidx_net} = 1;
+						$pins[$h_outpinId_idx{$powerNet}][1] = $netName."_".$subidx_net;
+						$pins[$h_pinId_idx{$source_ofNet}][1] = $netName."_".$subidx_net;
+						$pins[$h_pinId_idx{$source_ofNet}][2] = "s";
+						print "  $netName\_$subidx_net => $powerNet $source_ofNet (will be removed)\n";
+						$idx_nets++;
+						$subidx_net++;
+						## Generate Instance Information for applying DDA
+						if($pins[$h_pinId_idx{$source_ofNet}][7] eq "S"){
+							my $instIdx = $h_inst_idx{$pins[$h_pinId_idx{$source_ofNet}][6]};
+							my @tmp_finger = getAvailableNumFinger($inst[$instIdx][2], $trackEachPRow);
+							my $FlipFlag = 0;
+							if($tmp_finger[0]%2 == 0){
+								$FlipFlag = 2;
+							}
+							# DDA = (instanceID, FlipFlag)
+							if($instIdx <= $lastIdxPMOS){
+								push(@DDA_PMOS, [($instIdx, $FlipFlag)]);
+								$numPowerPmos++;
+							}
+							else{
+								push(@DDA_NMOS, [($instIdx, $FlipFlag)]);
+								$numPowerNmos++;
+							}
+						}
+						elsif($pins[$h_pinId_idx{$source_ofNet}][7] eq "D"){
+							my $instIdx = $h_inst_idx{$pins[$h_pinId_idx{$source_ofNet}][6]};
+							my @tmp_finger = getAvailableNumFinger($inst[$instIdx][2], $trackEachPRow);
+							my $FlipFlag = 0;
+							if($tmp_finger[0]%2 == 1){
+								$FlipFlag = 1;
+							}
+							else{
+								next;
+							}
+							# DDA = (instanceID, FlipFlag)
+							if($instIdx <= $lastIdxPMOS){
+								push(@DDA_PMOS, [($instIdx, $FlipFlag)]);
+								$numPowerPmos++;
+							}
+							else{
+								push(@DDA_NMOS, [($instIdx, $FlipFlag)]);
+								$numPowerNmos++;
+							}
+						}
+					}
+				}
+			}
+        }
+    }
 }
+
+# pin hash arrays
+print "******************************************\n";
+print "*** h_pin_id: ***\n", Dumper(\%h_pin_id);
+print "*** h_pin_idx: ***\n", Dumper(\%h_pin_idx);
+print "*** h_pinId_idx: ***\n", Dumper(\%h_pinId_idx);
+print "*** h_outpinId_idx : ***\n", Dumper(\%h_outpinId_idx );
+print "*** h_pin_net : ***\n", Dumper(\%h_pin_net );
+
+# net hash arrays
+print "******************************************\n"; 
+print "*** h_extnets : ***\n", Dumper(\%h_extnets);
+print "*** h_idx : ***\n", Dumper(\%h_idx );
+print "*** h_outnets : ***\n", Dumper(\%h_outnets );
+print "*** DDA_PMOS : ***\n", Dumper(\@DDA_PMOS);
+print "*** DDA_NMOS : ***\n", Dumper(\@DDA_NMOS);
+print "*** inst_group : ***\n", Dumper(\@inst_group);
+
+# instance hash arrays
+print "******************************************\n";
+print "*** h_inst_idx : ***\n", Dumper(\%h_inst_idx );
+print "*** numFinger  : ***\n", Dumper(\@numFinger  );
+print "*** h_inst_group : ***\n", Dumper(\%h_idx );
+print "*** inst_group_p  : ***\n", Dumper(\@inst_group_p );
+print "*** inst_group_n   : ***\n", Dumper(\@inst_group_n  );
+
+=pod
+*** h_outnets : ***
+$VAR1 = {
+          'net4' => 1
+        };
+=cut
